d2f852f0394948380d9cf124a70b7dfd745e5343
K <kwailamchan@hotmail.com>
2020-03-16 12:23:23 +0800

    [master] creating 04_video_creator. 	modified:   ../.gitignore 	new file:   main.py 	new file:   video.py 	modified:   ../environment.yml

---
+++ 04_video_creator/video.py
@@ -0,0 +1,25 @@
+import cv2
+from cv2 import VideoWriter, VideoWriter_fourcc
+
+
+class Video(object):
+
+    def __init__(self, filename,
+                       width=1280,
+                       height=720,
+                       fps=24, seconds=10,
+                       videotype='MP4V'):
+
+        self._width = width
+        self._height = height
+        self._fps = fps
+        self._seconds = seconds
+
+        fourcc = VideoWriter_fourcc(*videotype)
+        self._video = VideoWriter(filename,
+                                  fourcc,
+                                  float(self._fps),
+                                  (self._width, self._height))
+
+    def get_video(self):
+        return self._video
