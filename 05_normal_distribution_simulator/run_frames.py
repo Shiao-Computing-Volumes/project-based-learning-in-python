41dfd27b07d0b5313e62be18ec8da3d674c76062
K <kwailamchan@hotmail.com>
2020-03-17 21:56:59 +0800

    [master] removing normal.py. 	deleted:    normal.py 	modified:   run_frames.py

--- 05_normal_distribution_simulator/run_frames.py
+++ 05_normal_distribution_simulator/run_frames.py
@@ -1,7 +1,25 @@
 import sys
 sys.path.insert(0, ".")
 
-from normal import normal_plot
+from studio.frames.frames_writer import frames_writer
+from studio.charting.histograms import hist_density
+from studio.datasets.sampling import normal_distribution
+
+
+def normal_plot(params, outfile):
+    datasets = []
+    caption1_fmt = r"$n = {}, \bar x = {}, s = {}$"
+    captions1 = []
+    for param in params:
+        n, mu, sigma = param[0], param[1], param[2]
+        datasets.append(normal_distribution(mu, sigma, n))
+        captions1.append(caption1_fmt.format(n, mu, sigma))
+
+    suptitle = "Normal Distribution"
+    title = "{}"
+    caption2 = r"$P(x) = \frac{1}{\sigma\sqrt{2\pi}}e^{-(x-\mu)^2/(2\sigma^2)}$"
+    animation = hist_density(datasets, suptitle, title, captions1, caption2)
+    frames_writer(animation, outfile)
 
 
 if __name__ == '__main__':
